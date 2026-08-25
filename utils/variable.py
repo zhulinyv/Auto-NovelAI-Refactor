97cc8a7fd40b9bdabb68b062b863ba43a0fd124e
zhulinyv <zhulinyv2005@outlook.com>
2026-08-25 19:20:48 +0800

    :bento: 跟进数据

--- utils/variable.py
+++ utils/variable.py
@@ -2,7 +2,7 @@ import os
 
 from utils.environment import env
 
-VERSION = "1.7.2.preview2"
+VERSION = "1.7.2.preview3"
 
 MODELS = [
     "nai-diffusion-5-full",
@@ -53,6 +53,17 @@ CR_MODE = ["character&style", "character", "style"]
 
 BASE_PATH = os.getcwd()
 
+_SKIP_CFG_ABOVE_SIGMA = {
+    "nai-diffusion-5-full": None,
+    "nai-diffusion-5-curated": None,
+    "nai-diffusion-4-5-full": 58,
+    "nai-diffusion-4-5-curated": 58,
+    "nai-diffusion-4-full": 19,
+    "nai-diffusion-4-curated-preview": 16.92517469515569,
+    "nai-diffusion-3": 9.36441710371274,
+    "nai-diffusion-furry-3": 11.84515480302779,
+}
+
 
 if env.proxy:
     proxies = {
@@ -64,19 +75,7 @@ else:
 
 
 def return_skip_cfg_above_sigma(model):
-    if model in ["nai-diffusion-5-full", "nai-diffusion-5-curated"]:
-        value = None
-    elif model == "nai-diffusion-4-5-full":
-        value = 58
-    elif model == "nai-diffusion-4-5-curated":
-        value = 36.158893609242725
-    elif model in ["nai-diffusion-4-full"]:
-        value = 19
-    elif model in ["nai-diffusion-3"]:
-        value = 19.343056794463642
-    elif model in ["nai-diffusion-furry-3", "nai-diffusion-4-curated-preview"]:
-        value = 11.84515480302779
-    return value
+    return _SKIP_CFG_ABOVE_SIGMA.get(model)
 
 
 def return_uc_preset_id(model):
@@ -98,24 +97,24 @@ def return_uc_preset_id(model):
 def return_undesired_contentc_preset(model, undesired_contentc_preset):
     presets = {
         "nai-diffusion-5-full": {
-            "Heavy": "lowres, artistic error, film grain, scan artifacts, worst quality, bad quality, jpeg artifacts, very displeasing, chromatic aberration, dithering, halftone, screentone, multiple views, logo, too many watermarks, negative space, blank page",
-            "Light": "lowres, artistic error, scan artifacts, worst quality, bad quality, jpeg artifacts, multiple views, very displeasing, too many watermarks, negative space, blank page",
-            "Furry Focus": "{worst quality}, distracting watermark, unfinished, bad quality, {widescreen}, upscale, {sequence}, {{grandfathered content}}, blurred foreground, chromatic aberration, sketch, everyone, [sketch background], simple, [flat colors], ych (character), outline, multiple scenes, [[horror (theme)]], comic",
-            "Human Focus": "lowres, artistic error, film grain, scan artifacts, worst quality, bad quality, jpeg artifacts, very displeasing, chromatic aberration, dithering, halftone, screentone, multiple views, logo, too many watermarks, negative space, blank page",
+            "Heavy": "nsfw, lowres, artistic error, film grain, scan artifacts, worst quality, bad quality, jpeg artifacts, very displeasing, chromatic aberration, dithering, halftone, screentone, multiple views, logo, too many watermarks, negative space, blank page",
+            "Light": "nsfw, lowres, bad hands, bad anatomy, artistic error, sepia, white haze, worst quality, very displeasing, jpeg artifacts, 0::ai-generated::",
+            "Furry Focus": "nsfw, {worst quality}, distracting watermark, unfinished, bad quality, {widescreen}, upscale, {sequence}, {{grandfathered content}}, blurred foreground, chromatic aberration, sketch, everyone, [sketch background], simple, [flat colors], ych (character), outline, multiple scenes, [[horror (theme)]], comic",
+            "Human Focus": "nsfw, lowres, artistic error, film grain, scan artifacts, worst quality, bad quality, jpeg artifacts, very displeasing, chromatic aberration, dithering, halftone, screentone, multiple views, logo, too many watermarks, negative space, blank page, @_@, mismatched pupils, glowing eyes, bad anatomy",
             "None": "",
         },
         "nai-diffusion-5-curated": {
             "Heavy": "lowres, artistic error, film grain, scan artifacts, worst quality, bad quality, jpeg artifacts, very displeasing, chromatic aberration, dithering, halftone, screentone, multiple views, logo, too many watermarks, negative space, blank page",
-            "Light": "lowres, artistic error, scan artifacts, worst quality, bad quality, jpeg artifacts, multiple views, very displeasing, too many watermarks, negative space, blank page",
+            "Light": "lowres, bad hands, bad anatomy, artistic error, sepia, white haze, worst quality, very displeasing, jpeg artifacts, 0::ai-generated::",
             "Furry Focus": "{worst quality}, distracting watermark, unfinished, bad quality, {widescreen}, upscale, {sequence}, {{grandfathered content}}, blurred foreground, chromatic aberration, sketch, everyone, [sketch background], simple, [flat colors], ych (character), outline, multiple scenes, [[horror (theme)]], comic",
-            "Human Focus": "lowres, artistic error, film grain, scan artifacts, worst quality, bad quality, jpeg artifacts, very displeasing, chromatic aberration, dithering, halftone, screentone, multiple views, logo, too many watermarks, negative space, blank page",
+            "Human Focus": "lowres, artistic error, film grain, scan artifacts, worst quality, bad quality, jpeg artifacts, very displeasing, chromatic aberration, dithering, halftone, screentone, multiple views, logo, too many watermarks, negative space, blank page, @_@, mismatched pupils, glowing eyes, bad anatomy",
             "None": "",
         },
         "nai-diffusion-4-5-full": {
-            "Heavy": "lowres, artistic error, film grain, scan artifacts, worst quality, bad quality, jpeg artifacts, very displeasing, chromatic aberration, dithering, halftone, screentone, multiple views, logo, too many watermarks, negative space, blank page",
-            "Light": "lowres, artistic error, scan artifacts, worst quality, bad quality, jpeg artifacts, multiple views, very displeasing, too many watermarks, negative space, blank page",
-            "Furry Focus": "{worst quality}, distracting watermark, unfinished, bad quality, {widescreen}, upscale, {sequence}, {{grandfathered content}}, blurred foreground, chromatic aberration, sketch, everyone, [sketch background], simple, [flat colors], ych (character), outline, multiple scenes, [[horror (theme)]], comic",
-            "Human Focus": "lowres, artistic error, film grain, scan artifacts, worst quality, bad quality, jpeg artifacts, very displeasing, chromatic aberration, dithering, halftone, screentone, multiple views, logo, too many watermarks, negative space, blank page, @_@, mismatched pupils, glowing eyes, bad anatomy",
+            "Heavy": "nsfw, lowres, artistic error, film grain, scan artifacts, worst quality, bad quality, jpeg artifacts, very displeasing, chromatic aberration, dithering, halftone, screentone, multiple views, logo, too many watermarks, negative space, blank page",
+            "Light": "nsfw, lowres, artistic error, scan artifacts, worst quality, bad quality, jpeg artifacts, multiple views, very displeasing, too many watermarks, negative space, blank page",
+            "Furry Focus": "nsfw, {worst quality}, distracting watermark, unfinished, bad quality, {widescreen}, upscale, {sequence}, {{grandfathered content}}, blurred foreground, chromatic aberration, sketch, everyone, [sketch background], simple, [flat colors], ych (character), outline, multiple scenes, [[horror (theme)]], comic",
+            "Human Focus": "nsfw, lowres, artistic error, film grain, scan artifacts, worst quality, bad quality, jpeg artifacts, very displeasing, chromatic aberration, dithering, halftone, screentone, multiple views, logo, too many watermarks, negative space, blank page, @_@, mismatched pupils, glowing eyes, bad anatomy",
             "None": "",
         },
         "nai-diffusion-4-5-curated": {
@@ -125,8 +124,8 @@ def return_undesired_contentc_preset(model, undesired_contentc_preset):
             "None": "",
         },
         "nai-diffusion-4-full": {
-            "Heavy": "blurry, lowres, error, film grain, scan artifacts, worst quality, bad quality, jpeg artifacts, very displeasing, chromatic aberration, multiple views, logo, too many watermarks, white blank page, blank page",
-            "Light": "blurry, lowres, error, worst quality, bad quality, jpeg artifacts, very displeasing, white blank page, blank page",
+            "Heavy": "nsfw, blurry, lowres, error, film grain, scan artifacts, worst quality, bad quality, jpeg artifacts, very displeasing, chromatic aberration, multiple views, logo, too many watermarks, white blank page, blank page",
+            "Light": "nsfw, blurry, lowres, error, worst quality, bad quality, jpeg artifacts, very displeasing, white blank page, blank page",
             "None": "",
         },
         "nai-diffusion-4-curated-preview": {
@@ -135,14 +134,14 @@ def return_undesired_contentc_preset(model, undesired_contentc_preset):
             "None": "",
         },
         "nai-diffusion-3": {
-            "Heavy": "lowres, {bad}, error, fewer, extra, missing, worst quality, jpeg artifacts, bad quality, watermark, unfinished, displeasing, chromatic aberration, signature, extra digits, artistic error, username, scan, [abstract]",
-            "Light": "lowres, jpeg artifacts, worst quality, watermark, blurry, very displeasing",
-            "Human Focus": "lowres, {bad}, error, fewer, extra, missing, worst quality, jpeg artifacts, bad quality, watermark, unfinished, displeasing, chromatic aberration, signature, extra digits, artistic error, username, scan, [abstract], bad anatomy, bad hands, @_@, mismatched pupils, heart-shaped pupils, glowing eyes",
-            "None": "",
+            "Heavy": "nsfw, lowres, {bad}, error, fewer, extra, missing, worst quality, jpeg artifacts, bad quality, watermark, unfinished, displeasing, chromatic aberration, signature, extra digits, artistic error, username, scan, [abstract]",
+            "Light": "nsfw, lowres, jpeg artifacts, worst quality, watermark, blurry, very displeasing",
+            "Human Focus": "nsfw, lowres, {bad}, error, fewer, extra, missing, worst quality, jpeg artifacts, bad quality, watermark, unfinished, displeasing, chromatic aberration, signature, extra digits, artistic error, username, scan, [abstract], bad anatomy, bad hands, @_@, mismatched pupils, heart-shaped pupils, glowing eyes",
+            "None": "lowres",
         },
         "nai-diffusion-furry-3": {
-            "Heavy": "{{worst quality}}, [displeasing], {unusual pupils}, guide lines, {{unfinished}}, {bad}, url, artist name, {{tall image}}, mosaic, {sketch page}, comic panel, impact (font), [dated], {logo}, ych, {what}, {where is your god now}, {distorted text}, repeated text, {floating head}, {1994}, {widescreen}, absolutely everyone, sequence, {compression artifacts}, hard translated, {cropped}, {commissioner name}, unknown text, high contrast",
-            "Light": "{worst quality}, guide lines, unfinished, bad, url, tall image, widescreen, compression artifacts, unknown text",
+            "Heavy": "nsfw, {{worst quality}}, [displeasing], {unusual pupils}, guide lines, {{unfinished}}, {bad}, url, artist name, {{tall image}}, mosaic, {sketch page}, comic panel, impact (font), [dated], {logo}, ych, {what}, {where is your god now}, {distorted text}, repeated text, {floating head}, {1994}, {widescreen}, absolutely everyone, sequence, {compression artifacts}, hard translated, {cropped}, {commissioner name}, unknown text, high contrast",
+            "Light": "nsfw, {worst quality}, guide lines, unfinished, bad, url, tall image, widescreen, compression artifacts, unknown text",
             "None": "",
         },
     }
@@ -164,38 +163,38 @@ def return_quality_preset_id(model):
 def return_quality_tags(model, quality_tags_preset):
     presets = {
         "nai-diffusion-5-full": {
-            "Standard": "",
-            "Light": "",
+            "Standard": "very aesthetic, masterpiece, no text",
+            "Light": "very aesthetic, amazing quality, no text",
             "None": "",
         },
         "nai-diffusion-5-curated": {
-            "Standard": "",
-            "Light": "",
+            "Standard": "very aesthetic, masterpiece, no text",
+            "Light": "very aesthetic, amazing quality, no text",
             "None": "",
         },
         "nai-diffusion-4-5-full": {
-            "Standard": "",
+            "Standard": "very aesthetic, masterpiece, no text",
             "None": "",
         },
         "nai-diffusion-4-5-curated": {
-            "Standard": "",
+            "Standard": "very aesthetic, masterpiece, no text, -0.8::feet::, rating:general",
             "None": "",
         },
         "nai-diffusion-4-full": {
-            "Standard": "",
+            "Standard": "no text, best quality, very aesthetic, absurdres",
             "None": "",
         },
         "nai-diffusion-4-curated-preview": {
-            "Standard": "",
+            "Standard": "rating:general, best quality, very aesthetic, absurdres",
             "None": "",
         },
         "nai-diffusion-3": {
-            "Standard": "",
+            "Standard": "best quality, amazing quality, very aesthetic, absurdres",
             "None": "",
         },
         "nai-diffusion-furry-3": {
-            "Standard": "",
-            "None": "",
+            "Standard": "{best quality}, {amazing quality}",
+            "None": "lowres",
         },
     }
     return presets.get(model, {}).get(quality_tags_preset, "")
